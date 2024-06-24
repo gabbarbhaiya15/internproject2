a9887bd5781fd42f3b0ab804dfa8ade4859b435a
gabbarbhaiya15 <143590849+gabbarbhaiya15@users.noreply.github.com>
2024-06-24 15:22:09 +0530

    Update Alluser.js

--- Frontend/src/Component/Alluser.js
+++ Frontend/src/Component/Alluser.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Loader from './Loader'; // Import the Loader component
 import '../Style/Alluser.css';
 
 const Alluser = () => {
     const [Data, setData] = useState([]);
+    const [loading, setLoading] = useState(true); // State to manage loading
 
     useEffect(() => {
         axios.get("https://internproject2.onrender.com/Alluser", { withCredentials: true })
             .then((res) => {
                 console.log(res.data);
                 setData(res.data);
+                setLoading(false); // Data fetched, set loading to false
             })
             .catch((err) => {
                 console.error("Error fetching data: ", err);
+                setLoading(false); // Error occurred, set loading to false
             });
     }, []);
 
@@ -31,7 +35,9 @@ const Alluser = () => {
 
     return (
         <div className='All_user'>
-            {
+            {loading ? (
+                <Loader /> // Show loader while loading
+            ) : (
                 Data.map((item) => (
                     <div className='profile_card' key={item._id}>
                         <img src={item.avatar} alt='Avatar' className='avatar' />
@@ -43,7 +49,7 @@ const Alluser = () => {
                         <button className="add_users" onClick={() => collectdata(item._id)}>Add Team</button>
                     </div>
                 ))
-            }
+            )}
         </div>
     );
 };
